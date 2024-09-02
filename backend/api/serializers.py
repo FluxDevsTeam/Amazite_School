61408cdf76403aa7212af61b9489d61005e90c5e
Anyiamjoel <anyiamchiemelie5@gmail.com>
2024-09-02 12:41:55 +0100

    added news and events

---
+++ backend/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from event_and_award.models import Events
+from news_and_stories.models import News
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Events
+        fields=['id', 'title', 'body', 'image']
+
+
+
+
+class NewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=News
+        fields=['id', 'title','body','image']
--- backend/news_and_stories/models.py
+++ backend/news_and_stories/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class News(models.Model):
+    title=models.CharField(max_length=250)
+    body=models.TextField()
+    image = models.ImageField(upload_to='images/' , blank=False)
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
