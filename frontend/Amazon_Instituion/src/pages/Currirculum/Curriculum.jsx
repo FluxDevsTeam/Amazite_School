d1ca4288f46948138b521339e2847ed5cd6bc51b
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-09-18 19:42:08 +0100

    fix/ resolve missing page

--- frontend/Amazon_Instituion/src/pages/Currirculum/Curriculum.jsx
+++ frontend/Amazon_Instituion/src/pages/Currirculum/Curriculum.jsx
@@ -1,19 +1,7 @@
 const Curriculum = () => {
   return (
     <div className="text-8xl text-teal-600 flex items-center justify-center">
-      <button
-        class="relative inline-flex items-center justify-center px-8 py-4 font-medium text-4xl text-[#F3F0E7] rounded-full"
-        style="
-      background: linear-gradient(180deg, #FF7E35 0%, #FF5C00 100%);
-      box-shadow:
-        inset 0 3px 0 rgba(255, 255, 255, 0.22),
-        inset 0 26px 23px rgba(255, 255, 255, 0.59),
-        inset 0 -12px -6px rgba(0, 0, 0, 0.59),
-        inset 0 -29px -8px rgba(0, 0, 0, 0.29);
-    "
-      >
-        @MarkKnd
-      </button>
+      Curriculum
     </div>
   );
 };
