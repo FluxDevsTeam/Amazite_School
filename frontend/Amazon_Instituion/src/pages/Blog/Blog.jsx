030e10e2809414837d2eaf019b164cbefcccd8b1
Tryb3-Tunchi <bebefinn72@gmail.com>
2024-09-28 03:25:47 -0400

    updated blogs

--- frontend/Amazon_Instituion/src/pages/Blog/Blog.jsx
+++ frontend/Amazon_Instituion/src/pages/Blog/Blog.jsx
@@ -1,15 +1,22 @@
-import SocialIcons from '../../components/SocialIcons';
-import Articles from './Articles';
-import Events from './Events';
-import News from './News';
+import { useState } from "react";
+import SocialIcons from "../../components/SocialIcons";
+import Articles from "./Articles";
+import Events from "./Events";
+import News from "./News";
 
 const Blog = () => {
+  const [email, setEmail] = useState();
+  const submitEmail = (event) => {
+    event.preventDefault();
+    setEmail("");
+  };
+
   return (
     <div className="w-10/12 m-auto my-10">
       <div className="text-center tracking-wide leading-10">
         <div className="text-xl text-teal-600 ">
-          {' '}
-          Blog: User stories and student perfomance{' '}
+          {" "}
+          Blog: User stories and student perfomance{" "}
         </div>
         <p>subscribe to our news letter to see latest news</p>
       </div>
@@ -18,8 +25,13 @@ const Blog = () => {
           className="p-2 mr-2 mb-2  rounded-lg border"
           type="email"
           placeholder="Enter email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
-        <button className="bg-teal-600 text-white p-1 my- px-2 rounded-md">
+        <button
+          onClick={submitEmail}
+          className="bg-teal-600 text-white p-1 my- px-2 rounded-md"
+        >
           Enter
         </button>
       </div>
