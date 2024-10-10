64e764caa57782aec2db93fff182727a9db38200
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-10-10 21:21:05 +0100

    corrected staticfiles url

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
